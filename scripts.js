9ba2942502e12a0a1d5be103cc1e455634a8a82e
Gading Kinarayana <100770471+Gadingknr@users.noreply.github.com>
2023-03-20 17:32:14 +0800

    add more

--- scripts.js
+++ scripts.js
@@ -2,12 +2,15 @@
 var addTaskButton = document.getElementById("add-task")
 var addNewTaskInput = document.getElementById("task-input")
 var todoListcontainter = document.getElementById("todo-list")
+var templateContainer = document.getElementById("list-otem-template")
+var template = templateContainer.innerHTML
 
 //step 2 write the behaviour
 function onAddTaskClicked (event) {
     var taskName = addNewTaskInput.value;
     addNewTaskInput.value = "";
-    todoListcontainter.insertAdjacentElementHTML('afterbegin', taskName);
+    var taskHTML = template.replace("<-- TASK_NAME -->", taskName)
+    todoListcontainter.insertAdjacentElementHTML('afterbegin', taskHTML);
 }
 
 
