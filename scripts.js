cd69b659955554159020857cb9bbde18612ae195
Gading Kinarayana <100770471+Gadingknr@users.noreply.github.com>
2023-04-22 11:00:46 +0800

    fix java and html

--- scripts.js
+++ scripts.js
@@ -1,30 +1,32 @@
-//step 1 find the element
-var addTaskButton = document.getElementById("add-task")
-var addNewTaskInput = document.getElementById("task-input")
-var todoListcontainter = document.getElementById("todo-list")
-var templateContainer = document.getElementById("list-otem-template")
+// Step 1 find the element
+var addTaskButton = document.getElementById("add-task");
+var newTaskInput = document.getElementById("task-input");
+var todoListContainer = document.getElementById("todo-list");
+var templateContainer = document.getElementById("list-item-template");
 var template = templateContainer.innerHTML
 
-//step 2 write the behaviour
-function onAddTaskClicked (event) {
-    var taskName = addNewTaskInput.value;
-    addNewTaskInput.value = "";
-    var taskHTML = template.replace("<-- TASK_NAME -->", taskName)
-    todoListcontainter.insertAdjacentElementHTML('afterbegin', taskHTML);
+// Step 2 Write the behaviour
+function onAddTaskClicked(event) {
+    var taskName = newTaskInput.value;
+    newTaskInput.value = "";
+    var taskHTML = template.replace("<!-- TASK_NAME -->", taskName);
+    todoListContainer.insertAdjacentHTML('afterbegin', taskHTML);
 }
 
-function onTodoListContainer.insertAdjacentElementHTML(event){
-    while(!targetElement.classList.contains("task")){
-        targetElement = targetElement.parentElement;
-    }
-    var checkbox = targetElement.querySelector(".checkbox");
-    if (checkbox.checked){
-        targetElement.classList.remove("completed")
-    } else {
-        targetElement.classList.remove("completed")
-    }
+function onTodoListContainerClicked(event){
+   var targetElement = event.toElement;
+   
+   while (!targetElement.classList.contains("task")){
+      targetElement = targetElement.parentElement;
+   } 
+   
+   var checkbox = targetElement.querySelector(".checkbox");
+   if (checkbox.checked){
+      targetElement.classList.add("completed")
+   } else {
+      targetElement.classList.remove("completed")
+   }
 }
-
-//step 3 link the event handler
+// Step 3 link to event handler
 addTaskButton.addEventListener('click', onAddTaskClicked);
-todoListcontainter,addEventListener('click', onAddTaskClicked)
+todoListContainer.addEventListener('click', onTodoListContainerClicked);
